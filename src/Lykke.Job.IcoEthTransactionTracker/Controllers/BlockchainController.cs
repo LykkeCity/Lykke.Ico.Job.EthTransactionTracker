20fa28cd81876a21369568c09f1247bee043a35a
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2017-12-01 17:42:38 +0300

    Update packages, init entity metamodel

---
+++ src/Lykke.Job.IcoEthTransactionTracker/Controllers/BlockchainController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Job.IcoEthTransactionTracker.Controllers
+{
+    public class BlockchainController
+    {
+        // TODO: re-process block action (by height, maybe by hash)
+    }
+}
